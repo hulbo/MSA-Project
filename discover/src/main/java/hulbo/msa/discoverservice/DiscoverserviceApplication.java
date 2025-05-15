6ed50a4ee9d03b048b1bea346e21182d10022231
hulbo <hulbo@naver.com>
2025-05-15 15:13:02 +0900

    pom.xml 정의

--- discover/src/main/java/hulbo/msa/discoverservice/DiscoverserviceApplication.java
+++ discover/src/main/java/hulbo/msa/discoverservice/DiscoverserviceApplication.java
@@ -1,4 +1,4 @@
-package com.example.discoverservice;
+package hulbo.msa.discoverservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
